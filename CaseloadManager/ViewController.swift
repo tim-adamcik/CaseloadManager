9de8437a5ecf1f084509e45097e295651020a55c
Timothy Adamcik <tadamcik0425@gmail.com>
2020-07-17 11:22:24 -0400

    Initial Commit

---
+++ CaseloadManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CaseloadManager
+//
+//  Created by Timothy Adamcik on 7/17/20.
+//  Copyright © 2020 Timothy Adamcik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
